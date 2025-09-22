36bed8b072eb0bd4bd73b334bcba2b382bf81a54
Justin Lange <Justin.Lange@mpire.gmbh>
2025-09-22 10:48:05 +0200

    added a 3 second buffer to lock time in BLPOP  becasue server needs time to proccess request

--- app/BLPOP.py
+++ app/BLPOP.py
@@ -1,13 +1,13 @@
 import time
 from memory import MEMORY, BLOCKED_CLIENTS
-
+additionalTimeoutBuffer = 3 #add an additional buffer because server takes time to proccess 
 def BLPOP(clientConnection, command: list):
     try:
         listsToLock, lockDuration = command[1:-1], command[-1]
         for key in listsToLock:
             lockDurationInt = int(lockDuration)
             if lockDurationInt != 0 and lockDurationInt > 0:
-                lockExpiresAt = time.time() + lockDurationInt
+                lockExpiresAt = time.time() + lockDurationInt + additionalTimeoutBuffer
             else:
                 lockExpiresAt = 0
             if key not in MEMORY or MEMORY[key][0] == []:
